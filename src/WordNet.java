6c8ab378ec4d5857952b2a64e2c892e5c6185b53
Merouane Atig <merouane.atig@gmail.com>
2015-11-06 17:04:13 +0100

    Implement files reading for WordNet

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,22 +1,61 @@
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.LinearProbingHashST;
+import edu.princeton.cs.algs4.StdOut;
+
 
 
 public class WordNet {
+    private final LinearProbingHashST<String, Bag<Integer>> nouns =
+            new LinearProbingHashST<String, Bag<Integer>>();
+    private final Digraph G;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
         if (synsets == null) throw new NullPointerException();
         if (hypernyms == null) throw new NullPointerException();
+
+        In in = new In(synsets);
+        int V = 0;
+        while (in.hasNextLine()) {
+            V++;
+            String line = in.readLine();
+            String[] tokens = line.split(",");
+            int id = Integer.parseInt(tokens[0]);
+            String noun = tokens[1];
+            if (!nouns.contains(noun)) {
+                nouns.put(noun, new Bag<Integer>());
+            }
+            Bag<Integer> bag = nouns.get(noun);
+            bag.add(id);
+        }
+
+        in = new In(hypernyms);
+        G = new Digraph(V);
+        while (in.hasNextLine()) {
+            String line = in.readLine();
+            String[] tokens = line.split(",");
+            int synsetId = Integer.parseInt(tokens[0]);
+            for (int i = 1; i < tokens.length; i++) {
+                int w = Integer.parseInt(tokens[i]);
+                G.addEdge(synsetId, w);
+            }
+        }
+        StdOut.println(G.V());
+        StdOut.println(G.E());
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        return null;
+        return nouns.keys();
     }
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
         if (word == null) throw new NullPointerException();
-        return false;
+
+        return nouns.contains(word);
     }
 
     // distance between nounA and nounB (defined below)
@@ -36,5 +75,9 @@ public class WordNet {
 
     // do unit testing of this class
     public static void main(String[] args) {
+        String synsets = args[0];
+        String hypernyms = args[1];
+        WordNet wordNet = new WordNet(synsets, hypernyms);
+        StdOut.println(wordNet.isNoun("bird"));
     }
 }
