83c018c94431b240027fa9fa7ed50553eec5092a
Merouane Atig <merouane.atig@gmail.com>
2015-11-06 14:38:41 +0100

    Fix Oucast indentation

--- src/Outcast.java
+++ src/Outcast.java
@@ -2,17 +2,17 @@
 
 public class Outcast {
     // constructor takes a WordNet object
-    public Outcast(WordNet wordnet)         {
+    public Outcast(WordNet wordnet) {
 
     }
 
     // given an array of WordNet nouns, return an outcast
-    public String outcast(String[] nouns)   {
+    public String outcast(String[] nouns) {
         return null;
     }
 
     // see test client below
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
     }
 }
