c1a28944b50745b24d3a316bb1ad41179c0f3f45
Merouane Atig <merouane.atig@gmail.com>
2015-11-19 23:50:02 +0100

    Handle corner cases for removing seams and use spaces for indentation

--- seam_carving/src/SeamCarver.java
+++ seam_carving/src/SeamCarver.java
@@ -1,63 +1,87 @@
 import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
-	private final Picture picture;
+    private final Picture picture;
 
-	// create a seam carver object based on the given picture
-	public SeamCarver(Picture picture) {
-		checkNull(picture);
-		this.picture = new Picture(picture);
-	}
+    // create a seam carver object based on the given picture
+    public SeamCarver(Picture picture) {
+        checkNull(picture);
+        this.picture = new Picture(picture);
+    }
 
-	// current picture
-	public Picture picture() {
-		return picture;
-	}
+    // current picture
+    public Picture picture() {
+        return picture;
+    }
 
-	// width of current picture
-	public int width() {
-		return picture.width();
-	}
+    // width of current picture
+    public int width() {
+        return picture.width();
+    }
 
-	// height of current picture
-	public int height() {
-		return picture.height();
-	}
+    // height of current picture
+    public int height() {
+        return picture.height();
+    }
 
-	// energy of pixel at column x and row y
-	public double energy(int x, int y) {
-		if (x < 0 || x >= width()) {
-			throw new IndexOutOfBoundsException("x is out of bounds");
-		}
-		if (y < 0 || y >= height()) {
-			throw new IndexOutOfBoundsException("y is out of bounds");
-		}
-		return 0;
-	}
+    // energy of pixel at column x and row y
+    public double energy(int x, int y) {
+        if (x < 0 || x >= width()) {
+            throw new IndexOutOfBoundsException("x is out of bounds");
+        }
+        if (y < 0 || y >= height()) {
+            throw new IndexOutOfBoundsException("y is out of bounds");
+        }
+        return 0;
+    }
 
-	// sequence of indices for horizontal seam
-	public int[] findHorizontalSeam() {
-		return null;
-	}
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        return null;
+    }
 
-	// sequence of indices for vertical seam
-	public int[] findVerticalSeam() {
-		return null;
-	}
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        return null;
+    }
 
-	// remove horizontal seam from current picture
-	public void removeHorizontalSeam(int[] seam) {
-		checkNull(seam);
-	}
+    // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
+        checkNull(seam);
+        checkLength(seam, width());
+        checkValidSeam(seam);
+        if (height() <= 1) {
+            throw new IllegalArgumentException("Height is too small");
+        }
+    }
 
-	// remove vertical seam from current picture
-	public void removeVerticalSeam(int[] seam) {
-		checkNull(seam);
-	}
+    // remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam) {
+        checkNull(seam);
+        checkLength(seam, height());
+        checkValidSeam(seam);
+        if (width() <= 1) {
+            throw new IllegalArgumentException("Width is too small");
+        }
+    }
 
-	private void checkNull(Object o) {
-		if (o == null) {
-			throw new NullPointerException();
-		}
-	}
+    private void checkNull(Object o) {
+        if (o == null) {
+            throw new NullPointerException();
+        }
+    }
+
+    private void checkLength(int[] seam, int length) {
+        if (seam.length != length) {
+            throw new IllegalArgumentException("Array length is not valid");
+        }
+    }
+
+    private void checkValidSeam(int[] seam) {
+        for (int i = 1; i < seam.length; i++) {
+            if (Math.abs(seam[i] - seam[i-1]) > 1) {
+                throw new IllegalArgumentException("Seam is invalid");
+            }
+        }
+    }
 }
