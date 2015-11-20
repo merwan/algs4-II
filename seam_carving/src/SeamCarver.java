9f4d54f09b80bb98e262055c8b9d7ce55038440c
Merouane Atig <merouane.atig@gmail.com>
2015-11-20 10:55:59 +0100

    Return new picture in getter

--- seam_carving/src/SeamCarver.java
+++ seam_carving/src/SeamCarver.java
@@ -11,7 +11,7 @@ public class SeamCarver {
 
     // current picture
     public Picture picture() {
-        return picture;
+        return new Picture(picture);
     }
 
     // width of current picture
